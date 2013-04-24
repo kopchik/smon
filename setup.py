dc9b9cd91a2d1cb81001f3395936c11a6c952913
Alexandre Kandalintsev <spam@messir.net>
2013-04-24 11:01:56 +0300

    added systemd unit file and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='smon',
+      version='1.0',
+      author="Kandalintsev Alexandre",
+      author_email='spam@messir.net',
+      license="GPLv3",
+      description="reliable server monitoring daemon",
+      scripts=['smon.py'],
+      py_modules=["bottle"],
+      data_files=[('/etc/systemd/system/', ['smon.service'])]
+)
